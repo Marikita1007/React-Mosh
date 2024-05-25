87590f7750da270c877ab0676446ddbee73f5945
Marikita1007 <ishinomaki1007@gmail.com>
2024-05-25 12:58:41 +0200

    Alert using ReactNode

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,15 @@
 import React from 'react';
 import Message from "./Message";
-import ListGroup from "./components/ListGroup";
+import Alert from './components/Alert';
 
 function App() {
-
-    let items = [
-        'New York',
-        'Paris',
-        'Tokyo',
-        'London',
-        'Seoul'
-    ];
-
-    const handleSlectItem = (item: string) => {
-        console.log(item);
-    }
-
-
-    return <div><ListGroup items={items} heading="Cities" onSelectItem={handleSlectItem}/></div>
+    return (
+        <div>
+            <Alert>
+                Hello <span>World</span>
+            </Alert>
+        </div>
+    );
 }
 
 export default App;
